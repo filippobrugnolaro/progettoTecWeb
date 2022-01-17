8f52aa22270ccc82d2b7a8f2dda994d8bdf74492
Alessandro Cavaliere <ale.cava41@gmail.com>
2022-01-17 21:28:19 +0100

    predisposizione span per validazione JavaScript

--- contatti/contattiValidation.js
+++ contatti/contattiValidation.js
@@ -14,17 +14,15 @@ var validationDetails = {
 			var error = document.createElement("strong");
 			error.className = "errSuggestion";
 			error.appendChild(document.createTextNode(message));
-			
+
 			parent.appendChild(error);
 		}
-		
+
 		function fieldValidation(input) {
 			removeErrorMessage(input);
 			if(input.type !== "checkbox"){
                 if(input.value.search(validationDetails[input.id][1]) != 0 || input.value == validationDetails[input.id][0]) {
                     showError(input);
-                    //input.focus(); //focus on error (ok 4 users & SR)
-                    //input.select(); //select all chars
                     return false;
                 } else {
                     return true;
@@ -34,43 +32,27 @@ var validationDetails = {
 					return true;
 				} else {
 					showError(input);
-					//input.focus(); //focus on error (ok 4 users & SR)
-                    //input.select(); //select all chars
 					return false
 				}
 
 			}
 		}
-		
+
 		function removeErrorMessage(input) {
 			var parent = input.parentNode;
-			if(parent.children.length >= 2)
+			if(parent.children.length >= 2) {
 				parent.removeChild(parent.children[1]);
+			}
 		}
-		
+
 		function load() {
 			for(var key in validationDetails) {
 				var input = document.getElementById(key);
-				setDefault(input); //set placeholder
-				input.onfocus = function() {fieldForInput(this)}; //prepare field
+				input.onfocus = function() {removeErrorMessage(this)}; //prepare field
 				input.onblur = function() {fieldValidation(this)}; //validate field
 			}
 		}
-		
-		function setDefault(input) {
-			if(input.value == "") {
-				input.value = validationDetails[input.id][0];
-				input.className = "defaultText";
-			}
-		}
-		
-		function fieldForInput(input) {
-			if(input.value == validationDetails[input.id][0]) {
-				input.value = "";
-				input.className = "";
-			}
-		}
-		
+
 		function formValidation() {
 			var ret = true;
 			for(var key in validationDetails) {
