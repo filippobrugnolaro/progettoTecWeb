5eddacadbf4f00bb60518b9fae557729bc6752f2
Alessandro Cavaliere <ale.cava41@gmail.com>
2022-02-01 19:40:34 +0100

    modifiche

--- db.sql
+++ db.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 4.9.5deb2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Creato il: Gen 25, 2022 alle 23:31
--- Versione del server: 10.4.16-MariaDB
--- Versione PHP: 7.4.12
+-- Host: localhost:3306
+-- Creato il: Feb 01, 2022 alle 18:51
+-- Versione del server: 10.3.32-MariaDB-0ubuntu0.20.04.1
+-- Versione PHP: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -43,7 +44,13 @@ INSERT INTO `data_disponibile` (`data`, `posti`) VALUES
 ('2022-01-14', 200),
 ('2022-01-16', 100),
 ('2022-01-31', 2),
-('2022-02-22', 50);
+('2022-02-16', 50),
+('2022-02-19', 100),
+('2022-02-20', 100),
+('2022-02-22', 50),
+('2022-02-24', 50),
+('2022-02-26', 150),
+('2022-02-27', 150);
 
 -- --------------------------------------------------------
 
@@ -62,8 +69,8 @@ CREATE TABLE `ingressi_entrata` (
 --
 
 INSERT INTO `ingressi_entrata` (`codice`, `utente`, `data`) VALUES
-(43, 'CVLLSN00A04A001B', '2022-01-16'),
-(48, 'CVLLSN00A04A001B', '2022-01-31');
+(55, 'BRGFPP00B26C111Y', '2022-02-22'),
+(54, 'MMMGGG00A55G222V', '2022-02-16');
 
 -- --------------------------------------------------------
 
@@ -82,7 +89,9 @@ CREATE TABLE `ingressi_lezione` (
 --
 
 INSERT INTO `ingressi_lezione` (`codice`, `utente`, `lezione`) VALUES
-(11, 'CVLLSN00A04A001B', 6);
+(38, 'BRGFPP00B26C111Y', 11),
+(39, 'BRGFPP00B26C111Y', 15),
+(40, 'LDFDRE00P22A874G', 15);
 
 -- --------------------------------------------------------
 
@@ -104,8 +113,11 @@ CREATE TABLE `lezione` (
 --
 
 INSERT INTO `lezione` (`id`, `data`, `posti`, `descrizione`, `istruttore`, `pista`) VALUES
-(5, '2022-01-16', 15, 'Corso pro, allenamento mirato a migliorare il ritmo e il tempo sul giro secco. Inizio con analisi delle traiettorie suddividendo la pista per settori, successivamente simulazione qualifica e manche.', 'Antonio Cairoli', 20),
-(6, '2022-02-22', 1, 'Questo corso &egrave; un corso per insegnare', 'Filippo Brugnolaro', 22);
+(11, '2022-02-16', 5, 'Corso per neofiti. Si affronteranno i fondamentali della tecnica di guida: posizione sulla moto, staccata e tecnica in volo.', 'Filippo Brugnolaro', 2),
+(12, '2022-02-22', 5, 'Corso per amatori. Si divider&agrave; il tracciato in settori, che verranno analizzati singolarmente per capire le traiettorie ideali da utilizzare. A fine corso si far&agrave; una piccola simulazione di gara.', 'Leonardo Gambirasio', 1),
+(13, '2022-02-24', 4, 'Corso per professionisti. Si simuler&agrave; una giornata di gara nel suo complesso: prove libere, qualifiche, gara 1 e gara 2.', 'Riccardo Simionato', 1),
+(15, '2022-02-27', 2, 'Corso di enduro. Focalizza l\'attenzione sulle traiettorie da usare e la posizione in sella.', 'Filippo Brugnolaro', 3),
+(16, '2022-02-26', 5, 'Corso per neofiti, adatto a chi non &egrave; mai andato in moto.', 'Alessandro Cavaliere', 2);
 
 -- --------------------------------------------------------
 
@@ -115,11 +127,11 @@ INSERT INTO `lezione` (`id`, `data`, `posti`, `descrizione`, `istruttore`, `pist
 
 CREATE TABLE `messaggio` (
   `id` int(11) NOT NULL,
-  `nominativo` varchar(40) NOT NULL,
-  `email` varchar(30) NOT NULL,
+  `nominativo` varchar(60) NOT NULL,
+  `email` varchar(50) NOT NULL,
   `telefono` varchar(10) NOT NULL,
   `data` timestamp NOT NULL DEFAULT current_timestamp(),
-  `oggetto` varchar(30) NOT NULL,
+  `oggetto` varchar(100) NOT NULL,
   `testo` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -128,8 +140,8 @@ CREATE TABLE `messaggio` (
 --
 
 INSERT INTO `messaggio` (`id`, `nominativo`, `email`, `telefono`, `data`, `oggetto`, `testo`) VALUES
-(3, 'Ale Wwww', 'ale@ale.ale', '333333333', '2022-01-19 17:38:27', '333', '33333333333333333333333'),
-(4, 'Ale Cava', 'ale@ale.com', '3477625768', '2022-01-21 07:43:15', 's2', 'prova della prova1!!!');
+(7, 'Filippo Brugnolaro', 'filippo.brugnolaro.fb@gmail.com', '3459751230', '2022-02-01 17:16:26', 'Richiesta corso #15', 'Buongiorno, volevo chiedrvi se potevate per favore contattarmi per spiegarmi quale sia la difficolt&agrave; del corso in questione'),
+(9, 'Igor Zawalewski', 'igor@zawalewski.com', '3455734583', '2022-02-01 17:21:44', 'Richiesta informazioni noleggio', 'Buongiorno, volevo sapere se esiste qualche tipo di copertura assicurativa durante il noleggio');
 
 -- --------------------------------------------------------
 
@@ -151,7 +163,16 @@ CREATE TABLE `moto` (
 
 INSERT INTO `moto` (`numero`, `cilindrata`, `marca`, `modello`, `anno`) VALUES
 (13, 450, 'KTM', 'SXF 450', 2020),
-(14, 450, 'KTM', 'SXF 450', 2020);
+(14, 450, 'KTM', 'SXF 450', 2020),
+(16, 125, 'KTM', 'SX 125', 2020),
+(17, 450, 'HONDA', 'CRF 450', 2021),
+(18, 250, 'HONDA', 'CRF 250', 2022),
+(19, 250, 'KAWASAKI', 'KXF 250', 2019),
+(20, 450, 'KAWASAKI', 'KXF 450', 2021),
+(21, 250, 'SUZUKI', 'RMZ 250', 2018),
+(22, 125, 'YAMAHA', 'YZ 125', 2022),
+(23, 250, 'YAMAHA', 'YZF 250', 2021),
+(24, 250, 'HUSQVARNA', 'FC 250', 2022);
 
 -- --------------------------------------------------------
 
@@ -172,7 +193,10 @@ CREATE TABLE `noleggio` (
 --
 
 INSERT INTO `noleggio` (`codice`, `data`, `attrezzatura`, `utente`, `moto`) VALUES
-(26, '2022-01-16', 1, 'CVLLSN00A04A001B', 13);
+(44, '2022-02-22', 1, 'LDFDRE00P22A874G', 13),
+(45, '2022-02-22', 1, 'BRGFPP00B26C111Y', 21),
+(46, '2022-02-27', 0, 'BRGFPP00B26C111Y', 24),
+(47, '2022-02-27', 1, 'LDFDRE00P22A874G', 20);
 
 -- --------------------------------------------------------
 
@@ -195,9 +219,9 @@ CREATE TABLE `pista` (
 --
 
 INSERT INTO `pista` (`id`, `lunghezza`, `descrizione`, `terreno`, `apertura`, `chiusura`, `foto`) VALUES
-(20, 1000, 'Tracciato per esperti, grandi salti e ampie paraboliche! Serie di waves alternate da salti doppi e tripli.', 'terra_battuta', '12:20:00', '15:30:00', NULL),
-(21, 1000, 'Prova prova prova prova prova prova', 'terra_battuta', '08:00:00', '14:00:00', NULL),
-(22, 1000, 'Edeiuwhdiuehfdoiewhfoiwewedewdewde', 'sabbia', '12:30:00', '15:30:00', '22.png');
+(1, 1800, 'Tracciato pro, adatto solo ai piloti pi&ugrave; esperti. Si articola in lunghi panettoni, ampie curve paraboliche e salti doppi e tripli. Viene fresato raramente per simulare il pi&ugrave; possibile un tracciato di gara.', 'terra_morbida', '09:00:00', '17:00:00', '1.jpg'),
+(2, 1500, 'Tracciato easy, adatto a piloti neofiti e amatoriali. Si articola in panettoni di piccola e media lunghezza e curve principalmente senza sponde. Sono presenti molti canali e curve in contropendenza, per aiutare i piloti nell\'apprendimento della tecnica di guida.', 'terra_battuta', '10:00:00', '17:00:00', '2.jpg'),
+(3, 4200, 'Tracciato enduro. Adatto a piloti che non amano i salti o curve troppo impegnative. Si articola in lunghi rettilinei e ampie curve piatte.', 'terra_sassosa', '09:00:00', '15:00:00', '3.jpeg');
 
 -- --------------------------------------------------------
 
@@ -212,7 +236,7 @@ CREATE TABLE `utente` (
   `nome` varchar(25) NOT NULL,
   `nascita` date NOT NULL,
   `telefono` varchar(10) NOT NULL,
-  `email` varchar(35) NOT NULL,
+  `email` varchar(50) NOT NULL,
   `password` varchar(256) NOT NULL,
   `ruolo` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -222,10 +246,10 @@ CREATE TABLE `utente` (
 --
 
 INSERT INTO `utente` (`cf`, `username`, `cognome`, `nome`, `nascita`, `telefono`, `email`, `password`, `ruolo`) VALUES
-('CVLLSN00A04A001A', 'alecava41', 'Cavaliere', 'Alessandro', '2000-01-04', '3477625768', 'ac41husky@gmail.com', '$2y$10$ytTfwaIKaVl5YzK/CDBxYuUeBB41yAsmtjqoZWGs5UtYm2XJz.6.i', 2),
-('CVLLSN00A04A001B', 'aleuser41', 'Cavaliere', 'Alessandro', '2000-01-04', '3477625768', 'a@a.a', '$2y$10$szRaPrrGjqkejgTTPEzu0uWCjjV51i8LQ1La7QuX2XJzGffECXE/C', 1),
-('CVLLSN00A04A001E', 'andreiduk', 'Dukic', 'Andrei', '1999-12-12', '3477625768', 'andrei@dukic.com', '$2y$10$UfSWP6GZue3X0qQQTxf.rO69.OBpMai7F62o0.4evtGdOwKLrgAi6', 2),
-('CVLLSN00A04A001Q', 'mariorossi', 'Mario', 'Rossi', '2000-01-04', '3477625768', 'mario.rossi@gmail.com', '$2y$10$u8OPtYsmsqse1BqLesEJ9.5HVFe/6q.9dwXxNz53ONlr4I9dW1HWq', 1);
+('BRGFPP00B26C111Y', 'user', 'User', 'User', '2000-02-26', '3459751230', 'user@user.com', '$2y$10$OvvZ25M6vSs7QR1muPMtIuBCP7wkvbQjR4qCQJVbPFcAHs/...W/u', 1),
+('CVLLSN00A04A001A', 'admin', 'Admin', 'Admin', '2000-01-04', '3477625776', 'admin@amp.com', '$2y$10$iXw1Y0BTQYZ5dHZBinRNZ.Dr7AYohu/EIXLzGcQFmMVphGUqrTyTy', 2),
+('LDFDRE00P22A874G', 'leo22', 'Leo', 'Rossi', '2000-01-01', '3333333333', 'leo@gmail.com', '$2y$10$gqwJv8zfrJdSABRek4Gn7.aW6L1xWHd19zJyvWBYZIDSGU8yCADUi', 1),
+('MMMGGG00A55G222V', 'margre', 'Greggio', 'Marta', '2001-01-13', '3216571845', 'ciao@ciao.com', '$2y$10$w1AZTfSyZt2PJT6vHCwc9e72eBUUanUAVzQxiKtfWTbms/HUnS4T.', 1);
 
 --
 -- Indici per le tabelle scaricate
@@ -304,43 +328,43 @@ ALTER TABLE `utente`
 -- AUTO_INCREMENT per la tabella `ingressi_entrata`
 --
 ALTER TABLE `ingressi_entrata`
-  MODIFY `codice` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+  MODIFY `codice` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 
 --
 -- AUTO_INCREMENT per la tabella `ingressi_lezione`
 --
 ALTER TABLE `ingressi_lezione`
-  MODIFY `codice` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `codice` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT per la tabella `lezione`
 --
 ALTER TABLE `lezione`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT per la tabella `messaggio`
 --
 ALTER TABLE `messaggio`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT per la tabella `moto`
 --
 ALTER TABLE `moto`
-  MODIFY `numero` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `numero` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT per la tabella `noleggio`
 --
 ALTER TABLE `noleggio`
-  MODIFY `codice` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `codice` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- AUTO_INCREMENT per la tabella `pista`
 --
 ALTER TABLE `pista`
-  MODIFY `id` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- Limiti per le tabelle scaricate
